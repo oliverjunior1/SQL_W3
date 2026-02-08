a96cfa243d8295501f9544ea5d91bdd64e5019c2
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-08 17:32:54 -0300

    Create 9_select_top.sql

---
+++ 9_select_top.sql
@@ -0,0 +1,17 @@
+-- Syntax
+-- DELETE FROM table_name WHERE condition;
+
+DELETE FROM Customers 
+WHERE CustomerName = 'Alfreds Futterkiste';
+
+
+-- Delete all records
+
+DELETE FROM Customers;
+
+
+-- delete the customer table
+
+DROP TABLE Customers;
+
+   
