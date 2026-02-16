b4d6af864392d801216457b0a29161860b63cc8b
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-16 17:18:17 -0300

    Create 19_delete.sql

---
+++ 19_delete.sql
@@ -0,0 +1,5 @@
+-- DELETE FROM table_name WHERE condition;
+--The DELETE statement is used to delete existing records in a table.
+
+DELETE FROM Customers
+WHERE CustomerName = 'Alfreds Futterkiste';
