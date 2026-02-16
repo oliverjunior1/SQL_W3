42bb2f15d19839bcc19ecae6d51d074ecaa06264
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-16 17:20:22 -0300

    Update 19_delete.sql

--- 19_delete.sql
+++ 19_delete.sql
@@ -3,3 +3,9 @@
 
 DELETE FROM Customers
 WHERE CustomerName = 'Alfreds Futterkiste';
+
+-- The following SQL statement deletes all rows in the "Customers" table, without deleting the table:
+DELETE FROM Customers;
+
+-- To delete the table completely, use the DROP TABLE statement:
+DROP TABLE Customers;
