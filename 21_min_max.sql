b8d6b0bc2738ac6f5b6f90a2aea4cde3a14e287c
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-16 18:34:21 -0300

    Create 21_min_max.sql

---
+++ 21_min_max.sql
@@ -0,0 +1,36 @@
+-- The SELECT TOP clause is used to specify the number of records to return.
+SELECT TOP 3 * FROM Customers;
+
+SELECT * FROM Customers
+LIMIT 3;
+
+SELECT * FROM Customers
+FETCH FIRST 3 ROWS ONLY;
+
+SELECT TOP 50 PERCENT 
+FROM Customers;
+
+SELECT * FROM Customers 
+FETCH FIRST 50 PERCENT ROWS ONLY;
+
+SELECT TOP 3 * FROM Customers
+WHERE Country = 'Germany';
+
+SELECT * FROM Customers
+WHERE Country = 'Germany'
+LIMIT 3;
+
+SELECT * FROM Customers
+WHERE Country = 'Germany'
+FETCH FIRST 3 ROWS ONLY;
+
+SELECT TOP 3 * FROM Customers
+ORDER BY CustomerName DESC;
+
+SELECT * FROM Customers
+ORDER BY CustomerName DESC
+LIMIT 3;
+
+SELECT * FROM Customers
+ORDER BY CustomerName DESC
+FETCH FIRST 3 ROWS ONLY;
