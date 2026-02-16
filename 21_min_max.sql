c6b01dbe4d53a246a5c30b31ae3637a794f73681
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-16 18:45:43 -0300

    Update 21_min_max.sql

--- 21_min_max.sql
+++ 21_min_max.sql
@@ -1,5 +1,17 @@
+-- SELECT MIN(column_name)
+-- FROM table_name
+-- WHERE condition;
+
+-- SELECT MAX(column_name)
+-- FROM table_name
+-- WHERE condition;
+
 SELECT MIN(Price)
 FROM Products;
 
 SELECT MAX(Price)
 FROM Products;
+
+SELECT MIN(Price) AS SmallestPrice
+FROM Products;
+
