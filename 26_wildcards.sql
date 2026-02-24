0f43bc05cbdcc35a23afc5a45d6aafa8566b0689
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-24 19:31:52 -0300

    Update 26_wildcards.sql

--- 26_wildcards.sql
+++ 26_wildcards.sql
@@ -1,2 +1,13 @@
+/* Wildcard Characters
+Symbol	Description
+%	Represents zero or more characters
+_	Represents a single character
+[]	Represents any single character within the brackets *
+^	Represents any character not in the brackets *
+-	Represents any single character within the specified range *
+{}	Represents any escaped character **
+*/
+
 SELECT * FROM Customers
 WHERE CustomerName LIKE 'a%';
+
